489f1528a2f7f04d1bfbd523113034a289d604cf
Gwon Min Je <gwon713@gmail.com>
2021-08-17 00:47:15 +0900

    기능 수정 : 콘솔에서도 같은 포맷으로 출력되도록 설정 TIME ZONE은 컴퓨터에서 설정하자!

--- test_node/winston.js
+++ test_node/winston.js
@@ -3,18 +3,11 @@ const winstonDaily = require('winston-daily-rotate-file');
 
 const { combine, timestamp, printf } = winston.format;
 
-const moment = require('moment-timezone');
-const date = moment().tz('Asia/Seoul');
-const koreaTime = winston.format((info) => { // NOTE: 한국 시간으로 하기 위해.. 설정을 안 할 시 에는 UTC 0이 default다.
-    info.timestamp = date.format();
-    return info;
-});
 const logDir = 'logs';  // logs 디렉토리 하위에 로그 파일 저장
 
 const logFormat = printf(info => {
     return `${info.timestamp} ${info.level}: ${info.message}`;
 });
-
 /*
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
@@ -24,7 +17,6 @@ const logger = winston.createLogger({
         timestamp({
           format: 'YYYY-MM-DD HH:mm:ss',
         }),
-        koreaTime(),
         logFormat,
     ),
     transports: [
@@ -67,9 +59,9 @@ logger.stream = {// morgan wiston 설정
 // Production 환경이 아닌 경우(dev 등) 배포 환경에서는 최대한 자원을 안잡아 먹는 로그를 출력해야함
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
-        format: winston.format.combine(
-            winston.format.colorize(),  // 색깔 넣어서 출력
-            winston.format.simple(),  // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
+        format: combine(
+            colorize({ all: true }), // console 에 출력할 로그 컬러 설정 적용함
+            logFormat // log format 적용
         )
     }));
 }
